96828a0b689cc0b9f36a3c8ff96b881968152efb
seoi hong <90823683+hongseoi@users.noreply.github.com>
2022-05-03 14:56:40 +0900

    add data_structure

---
+++ data_struc/01-1.py
@@ -0,0 +1,20 @@
+
+def twoSum(nums, target):
+    nums.sort()
+    i = 0
+    j = len(nums)-1
+    
+    while i<j:
+        sum = nums[i] + nums[j]
+        if sum == target:
+            return nums[i], nums[j]
+        elif sum>target:
+            j -= 1
+        else:
+            i += 1
+            
+def main():
+    print(twoSum([2, 8, 19, 37, 4, 5], 12)) # (4, 8) 혹은 (8, 4)가 리턴되어야 합니다.
+
+if __name__ == "__main__":
+    main()
