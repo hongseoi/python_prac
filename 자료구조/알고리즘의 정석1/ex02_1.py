b473f520f66bcdc398cdb1c1e981c18677459d43
seoi hong <90823683+hongseoi@users.noreply.github.com>
2022-05-05 00:43:19 +0900

    Create new file

---
+++ 자료구조/알고리즘의 정석1/ex02_1.py
@@ -0,0 +1,34 @@
+import sys
+import math
+
+def getSubsum(data) :
+    '''
+    n개의 숫자가 list로 주어질 때, 그 연속 부분 최대합을 반환하는 함수를 작성하세요.
+    
+    모든 경우를 구하고 가장 큰 것을 출력
+    '''
+    result = -math.inf
+    
+    temp = 0
+    
+    for start in range(0, len(data)):
+        for end in range(start, len(data)):
+            temp = 0
+            for i in range(start, end+1):
+                temp += data[i]
+                
+            result = max(result, temp)
+    
+    return result
+
+def main():
+    '''
+    이 부분은 수정하지 마세요.
+    '''
+
+    data = [int(x) for x in input().split()]
+
+    print(getSubsum(data))
+
+if __name__ == "__main__":
+    main()
