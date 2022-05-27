e31c669b8cdc40e5f3dbe1b0d2619e991f3e66a2
seoi hong <90823683+hongseoi@users.noreply.github.com>
2022-05-27 22:32:55 +0900

    Create p1_2.py
    basic pygame

---
+++ PyGame/p1_2.py
@@ -0,0 +1,27 @@
+# 몫과 나머지 한번에 구하기
+from functools import total_ordering
+
+
+print(divmod(11,4))
+print(float(True))
+
+#bool 형으로 바꾸기: 0, 0.0, 빈 문자열은 False, 나머지는 True
+print(bool(''))
+print(bool("hello"))
+
+# 튜플: 생성 후 값 수정 불가능
+subject = (10,20,30,40)
+total = 0
+for i in range(4):
+    total += subject[i]
+print(total)
+
+# 리스트
+weekday = ['월','화','수', '목', '금','토']
+weekday.append('일')
+weekday.insert(2,'이요일')
+print(weekday)
+del weekday[2]
+print(weekday)
+weekday.pop(1)
+print(weekday)
