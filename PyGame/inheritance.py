00e6e653828a7355263f351ffe0a19f24f312e5f
seoi hong <90823683+hongseoi@users.noreply.github.com>
2022-07-17 00:45:55 +0900

    Update inheritance.py

---
+++ PyGame/inheritance.py
@@ -0,0 +1,29 @@
+class AudioVisual:
+    def __init__(self, power, volume):
+        self.power = power
+        self.volume = volume
+
+    def switch(self, on_off):
+        self.power = on_off
+    def set_volume(self, vol):
+        self.volume = vol
+
+class Audio(AudioVisual):
+    def __init__(self, power, volume): #생성자 생성
+        super().__init__(power, volume)
+    def tune(self):
+        str = "La la la..." if self.power else "turn it on"
+        print(str)
+
+class TV(AudioVisual):
+    def __init__(self, power, volume, size):
+        super().__init__(power, volume): #부모클래스 정의 참조
+        self.size = size
+
+    def watch(self):
+        str= "have fun!" if self.power else "switch on"
+        print(str)
+
+obj1 = TV(False, 12, 40)
+obj1.switch(True)
+obj1.watch()
